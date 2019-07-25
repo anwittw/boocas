286e013cecf295a546752be55d986ae8f628a196
Carlos Botto <carloscbotto@gmail.com>
2019-07-25 11:31:15 +0100

    group route

--- server/routes/group.js
+++ server/routes/group.js
@@ -4,6 +4,8 @@ const router = express.Router()
 
 const Group = require('../models/Group')
 
+// GET all
+
 router.get('/', (req, res, next) => {
   Group.find()
     .then(groups => {
@@ -14,11 +16,50 @@ router.get('/', (req, res, next) => {
     })
 })
 
+// GET by Id
+
 router.get('/:id', (req, res, next) => {
   let id = req.params.id
   Group.findbyId(id)
     .then(group => {
       res.json(group)
+    })
+    .catch(err => {
+      next({ status: 400, message: err })
+    })
+})
+
+// Give groups where bookid = :bookid
+
+router.get('/:bookId', (req, res, next) =>
+  Group.find({ _book: req.params.bookId }).populate('_book')
+)
+
+// Create a group
+
+router.post('/', (req, res, next) => {
+  Group.create({
+    isbn10: req.body.isbn_10,
+    isbn13: req.body.isbn_13,
+    title: req.body.title,
+    author: req.body.author,
+    year: req.body.year,
+  })
+    .then(group => {
+      next({ message: group })
+    })
+    .catch(err => {
+      next({ status: 400, message: err })
+    })
+})
+
+// DELETE one
+
+router.delete('/:id', (req, res, next) => {
+  let id = req.params.id
+  Group.findByIdAndDelete(id)
+    .then(group => {
+      next({ message: group })
     })
     .catch(err => {
       next({ status: 400, message: err })
