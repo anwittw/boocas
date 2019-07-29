b14517a0f0a3ce727b45e028ba31848ef2ceb8bc
anwittw <git@andrewittwer.de>
2019-07-29 18:07:23 +0100

    fixed main navbar

--- client/src/components/MainNavbar.jsx
+++ client/src/components/MainNavbar.jsx
@@ -9,16 +9,11 @@ function MainNavbar(props) {
   }
   return (
     <nav>
-<<<<<<< HEAD
-      <h1>bookbook</h1>
-      {/* <NavLink to="/" exact>
-=======
       <h1>{props.title}</h1>
       <NavLink to="/" exact>
->>>>>>> c7e569da1b91ef62fe0a3a38d5a7dd47e6691ddc
         Home
-      </NavLink> */}
-      {!api.isLoggedIn() && <NavLink to="/signup">Signup</NavLink>}
+      </NavLink>
+      !api.isLoggedIn() && <NavLink to="/signup">Signup</NavLink>}
       {!api.isLoggedIn() && <NavLink to="/login">Login</NavLink>}
       {api.isLoggedIn() && (
         <Link to="/" onClick={handleLogoutClick}>
