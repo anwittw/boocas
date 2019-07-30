b1bb7cc3ede72b48ef78d93af8df5f7b4f85e827
Ingo Grossmann <ingogrossmann@icloud.com>
2019-07-30 17:49:14 +0100

    Home-Screen

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   const [myMemberships, setMyMemberships] = useState([])
 
-  const [user, setUser] = useState('')
+  let username = JSON.parse(localStorage.getItem('user')).username
 
   useEffect(() => {
     api.getMyMemberships().then(response => {
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <div>
       <div className="App__right__header" style={{ padding: '15px 30px' }}>
-        <MainNavbar title={'Welcome'} />
+        <MainNavbar title={'Welcome ' + username} />
       </div>
       <span className="App__right__circle" />
 
@@ -27,47 +27,92 @@ export default function Home() {
           padding: '30px',
         }}
       >
-        {myMemberships.map(group => (
-          <Link to={'/group-detail/' + group._group._id}>
-            <div
-              className="hover card mb-3 bg-white border-secondary"
-              style={{
-                maxWidth: '500px',
-                padding: '0px 0px 20px 20px',
-              }}
+        {myMemberships
+          .sort(function(a, b) {
+            var Date1 = new Date(a._group.created_at)
+            var Date2 = new Date(b._group.created_at)
+
+            if (Date2 > Date1) return 1
+            if (Date1 < Date2) return -1
+          })
+          .map(group => (
+            <Link
+              to={'/group-detail/' + group._group._id}
+              key={group._group._id}
             >
-              <div className="row no-gutters">
-                <div className="col-md-4">
-                  <img
-                    src={group._group._book.coverPictureUrl}
-                    className="card-img"
-                    style={{ padding: '20px 0px 0px 0px' }}
-                    alt="..."
-                  />
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h5 className="card-title">
-                      <strong>{group._group.name}</strong>
-                    </h5>
-                    <hr />
-                    <p className="card-text">
-                      <small>{group._group.description}</small>
-                    </p>
-                    <p className="card-text">
-                      <small className="text-muted">
-                        Last updated 3 mins ago
-                      </small>
-                    </p>
+              <div
+                className="hover card mb-3 bg-white border-secondary"
+                style={{
+                  maxWidth: '500px',
+                  padding: '0px 0px 20px 20px',
+                }}
+              >
+                <div className="row no-gutters">
+                  <div className="col-md-4">
+                    <img
+                      src={group._group._book.coverPictureUrl}
+                      className="card-img"
+                      style={{ padding: '20px 20px 0px 0px' }}
+                      alt="..."
+                    />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        <strong>{group._group.name}</strong>
+                      </h5>
+                      <hr />
+                      <p className="card-text">
+                        <small>{group._group.description}</small>
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          ))}
       </div>
-
-      <pre>{JSON.stringify(myMemberships, null, 2)}</pre>
+      <div
+        className="card-columns"
+        style={{
+          padding: '30px',
+        }}
+      >
+        <Link to={'/create-group'}>
+          <div
+            className="hover card mb-3 bg-secondary"
+            style={{
+              maxWidth: '500px',
+              padding: '0px 0px 20px 20px',
+            }}
+          >
+            <div className="row no-gutters">
+              <div className="col-md-4">
+                <img
+                  src=""
+                  className="card-img"
+                  style={{ padding: '20px 20px 0px 0px' }}
+                  alt="..."
+                />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">
+                    <strong>Title</strong>
+                  </h5>
+                  <hr />
+                  <p className="card-text">
+                    <small>Description</small>
+                  </p>
+                  <p className="card-text">
+                    <small>Click to create a Group</small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Link>
+      </div>
     </div>
   )
 }
